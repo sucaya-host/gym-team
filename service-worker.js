2527be32e56ff5f49483f6bd40ddbabce0cc93ee
sucaya-host <sucayaputra8@gmail.com>
2026-02-22 17:37:43 +0800

    Refactor service worker cache management
    Updated cache name and added index.html to cache.

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,26 @@
+const CACHE_NAME = "gym-team-v2";
+
 self.addEventListener("install", function(event) {
   event.waitUntil(
-    caches.open("gym-team-cache").then(function(cache) {
+    caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/gym-team/"
+        "/gym-team/",
+        "/gym-team/index.html"
       ]);
+    })
+  );
+});
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(name) {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
     })
   );
 });
